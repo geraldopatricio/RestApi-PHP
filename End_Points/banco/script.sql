4a7639be88c5411a32f4363dfd53a44f31ef5e9d
Geraldo P Melo <geraldo@gpsoft.com.br>
2023-08-23 11:30:12 -0300

    Create script.sql

---
+++ End_Points/banco/script.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `garantia_motivo` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`descricao` VARCHAR(30) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	PRIMARY KEY (`id`) USING BTREE
+);
